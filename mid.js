8f1058a9db3ef2abcc91b5814a321f56650cf76d
Gujjula Devika <devika1303@gmail.com>
2026-09-01 20:15:34 +0530

    query selector

--- mid.js
+++ mid.js
@@ -1,6 +1,9 @@
 // JS_DOM
 let heading = document.getElementById("title");
 console.log(heading.innerText = "javascript DOM");
-// js_query selector
-let para = document.querySelector(".message");
-console.log(para.innerText = "devika");
+// // js_querySelector
+// let para = document.querySelector(".message");
+// console.log(para.innerText = "devika");
+// js_querySelectorAll
+let para = document.querySelectorAll(".message");
+console.log(para.innerText = "hello");
