2dff92dd2f66691c5c8e7efa8c4fc597e119e312
Gujjula Devika <devika1303@gmail.com>
2026-09-01 21:56:21 +0530

    dom with looping

--- mid.js
+++ mid.js
@@ -1,9 +1,17 @@
 // JS_DOM
 let heading = document.getElementById("title");
 console.log(heading.innerText = "javascript DOM");
+
 // // js_querySelector
 // let para = document.querySelector(".message");
 // console.log(para.innerText = "devika");
+
 // js_querySelectorAll
-let para = document.querySelectorAll(".message");
-console.log(para.innerText = "hello");
+let paragraph = document.querySelectorAll(".message");
+console.log(paragraph.innerText = "hello");
+
+// //querySelector with loops
+// let messages = document.querySelectorAll(".message");
+// for(let i = 0; i < messages.length; i++) {
+//     messages[i].innerText = "Hello";
+// }
