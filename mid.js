86ddac041340b1f3077afd80972a414374b81ceb
Gujjula Devika <devika1303@gmail.com>
2026-09-01 18:19:25 +0530

    DOM

---
+++ mid.js
@@ -0,0 +1,6 @@
+// JS_DOM
+let heading = document.getElementById("title");
+console.log(heading.innerText = "javascript DOM");
+// js_query selector
+let para = document.querySelector(".message");
+console.log(para.innerText = "devika");
