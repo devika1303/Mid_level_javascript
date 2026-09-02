120974640ca43fcc3e923eaa52277ab305c4f4bf
Gujjula Devika <devika1303@gmail.com>
2026-09-02 16:58:52 +0530

    dom manupulation-css in js

--- mid.js
+++ mid.js
@@ -1,17 +1,25 @@
-// JS_DOM
-let heading = document.getElementById("title");
-console.log(heading.innerText = "javascript DOM");
+// // JS_DOM
+// let heading = document.getElementById("title");
+// console.log(heading.innerText = "javascript DOM");
 
 // // js_querySelector
 // let para = document.querySelector(".message");
 // console.log(para.innerText = "devika");
 
-// js_querySelectorAll
-let paragraph = document.querySelectorAll(".message");
-console.log(paragraph.innerText = "hello");
+// // js_querySelectorAll
+// let paragraph = document.querySelectorAll(".message");
+// console.log(paragraph.innerText = "hello");
 
 // //querySelector with loops
 // let messages = document.querySelectorAll(".message");
 // for(let i = 0; i < messages.length; i++) {
 //     messages[i].innerText = "Hello";
 // }
+title.style.color = "red";
+title.style.backgroundColor = "black";
+
+let btn = document.getElementById("btn");
+let result = document.getElementById("heading2");
+btn.onclick = function(){
+    result.innerText = "Button Clicked!"
+}
